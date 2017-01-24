b17d7b258144f0532d392486c4c405622aecc817
BryanOrabutt <bryanorabutt@github.com>
2017-01-24 12:38:23 -0600

    added -f file option

--- matrix_mult/nxn-matrix.c
+++ matrix_mult/nxn-matrix.c
@@ -7,26 +7,26 @@
 int main(int argc, char** argv)
 {
     //initial variable declarations
-    int n, t, ch, row, col, m, max;
+    int n, t, ch, row, col, m, f, max;
     n = 0;
     max = 0;
+    f = 0;
 
     //create file handles and open files to store data points
     FILE* fid0, *fid1, *fid2, *fid3;
-    fid0 = (FILE*)fopen("c.txt", "w");
-    fid1 = (FILE*)fopen("b.txt", "w");
-    fid2 = (FILE*)fopen("a.txt", "w");
-    fid3 = (FILE*)fopen("result.txt", "w");
 
     //seed the random generator and set the option error variable
     srand(time(NULL));
     opterr = 1;
 
     //check for command line inputs.
-    while((ch = getopt(argc, argv, "m:n:t:")) != -1)
+    while((ch = getopt(argc, argv, "fm:n:t:")) != -1)
     {
         switch(ch)
         {
+	    case 'f':
+	       f = 1;
+	       break;
             case 'm':
                 max = atoi(optarg);
                 break;
@@ -43,7 +43,13 @@ int main(int argc, char** argv)
 
     if(!n) n = 4; //if n was not given use 4
     if(!max) max = 10; //if max not specified use 10.
-
+    if(f)
+    {
+        fid0 = (FILE*)fopen("c.txt", "w");
+        fid1 = (FILE*)fopen("b.txt", "w");
+        fid2 = (FILE*)fopen("a.txt", "w");
+        fid3 = (FILE*)fopen("result.txt", "w");
+    }
     //create matrices
     int a[n][n];
     int b[n][n];
@@ -57,9 +63,9 @@ int main(int argc, char** argv)
             a[row][col] = rand()%max;
             b[row][col] = rand()%max;
             c[row][col] = rand()%max;
-            fprintf(fid0, "%d\t", c[row][col]);
+           if(f)  fprintf(fid0, "%d\t", c[row][col]);
         }
-        fprintf(fid0, "\n");
+       if(f) fprintf(fid0, "\n");
     }
 
     //compute c = c + a*b
@@ -74,24 +80,27 @@ int main(int argc, char** argv)
         }
     }
 
-    //write results to a file.
-    for(row = 0; row < n; row++)
-    {
-        for(col = 0; col < n; col++)
+    if(f)
+   {
+        //write results to a file.
+        for(row = 0; row < n; row++)
         {
-            fprintf(fid3, "%d\t", c[row][col]);
-            fprintf(fid1, "%d\t", b[row][col]);
-            fprintf(fid2, "%d\t", a[row][col]);
+            for(col = 0; col < n; col++)
+            {
+                fprintf(fid3, "%d\t", c[row][col]);
+                fprintf(fid1, "%d\t", b[row][col]);
+                fprintf(fid2, "%d\t", a[row][col]);
+            }
+            fprintf(fid3, "\n");
+            fprintf(fid1, "\n");
+            fprintf(fid2, "\n");
         }
-        fprintf(fid3, "\n");
-        fprintf(fid1, "\n");
-        fprintf(fid2, "\n");
-    }
 
-    //close files.
-    fclose(fid0);
-    fclose(fid1);
-    fclose(fid2);
-    fclose(fid3);
+        //close files.
+        fclose(fid0);
+        fclose(fid1);
+        fclose(fid2);
+        fclose(fid3);
+    }
     return 0;
 }
